47f05d2156976d90fcd1ee99f667d157ce10cbe1
Kevin Glick <kevin.n.glick@gmail.com>
2017-03-12 11:24:46 -0400

    implement CRUD methods

--- controllers/tasks.go
+++ controllers/tasks.go
@@ -1,13 +1,91 @@
 package controllers
 
 import "net/http"
+import (
+	"github.com/Caoimhin89/first-go-app/models"
+	"github.com/Caoimhin89/first-go-app/common"
+	"encoding/json"
+	"github.com/gorilla/mux"
+)
 
 var Tasks = new(tasksController)
 
 type tasksController struct{}
 
-func (tc *tasksController) Create(w http.ResponseWriter, r *http.Request) {}
-func (tc *tasksController) Get(w http.ResponseWriter, r *http.Request) {}
-func (tc *tasksController) Show(w http.ResponseWriter, r *http.Request) {}
-func (tc *tasksController) Update(w http.ResponseWriter, r *http.Request) {}
-func (tc *tasksController) Delete(w http.ResponseWriter, r *http.Request) {}
+var t models.Task
+
+func (tc *tasksController) Create(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	task, err := models.Tasks.Create(t.Name, t.Desc)
+	if err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	res, err := json.Marshal(task)
+	if err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+	common.JsonValid(w, res, http.StatusCreated)
+}
+func (tc *tasksController) Get(w http.ResponseWriter, r *http.Request) {
+	tasks, err := models.Tasks.FindAll()
+	if err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	res, err := json.Marshal(tasks)
+	if err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+	common.JsonValid(w, res, http.StatusOK)
+}
+func (tc *tasksController) Show(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	task, err := models.Tasks.FindOne(id)
+	if err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	res, err := json.Marshal(task)
+	if err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+	common.JsonValid(w, res, http.StatusOK)
+}
+func (tc *tasksController) Update(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	if err := models.Tasks.Update(id, t.Name, t.Desc); err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+	common.JsonStatus(w, http.StatusNoContent)
+}
+func (tc *tasksController) Delete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if err := models.Tasks.DeleteById(id); err != nil {
+		common.JsonError(w, err, http.StatusBadRequest)
+		return
+	}
+	common.JsonStatus(w, http.StatusNoContent)
+}
